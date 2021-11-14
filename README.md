01b032cab8b6d3a9af313252be667e0a512d0104
Soumya Mukherjee <rivu10@gmail.com>
2021-11-14 13:11:10 +0530

    List and Map expression evaluation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Microservice-Configuration
+## It explains different configuration available in Spring Boot Project
+
+    1. Fetch properties value by adding @Value("${<<variable_name>>}")
+    2. Place application.properties file where jar is present and run it. It will overwrite the values from External Properties.
+    3. Mention default value in code if values are not mentioned in properties file i.e. @Value("${<<variable_name>>: <<default_value>>}")
+    
--- microservice-configuration/src/main/java/com/microservice/controller/SampleController.java
+++ microservice-configuration/src/main/java/com/microservice/controller/SampleController.java
@@ -1,5 +1,8 @@
 package com.microservice.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +15,15 @@ public class SampleController {
 	@Value("${user.greetingMessage: rivu}") // define default value
 	private String greetingMessage;
 	
+	@Value("${listValues}")
+	private List<String> listValues;
+	
+	@Value("#{${dbvalues}}") // evaluate spring expression by using # tag
+	private Map<String, String> dbvalues;
+	
 	@GetMapping("/")
 	public String getGreeting() {
-		return greetingMessage;
+		return greetingMessage + "\t" +listValues + "\t"+ dbvalues;
 	}
 
 }

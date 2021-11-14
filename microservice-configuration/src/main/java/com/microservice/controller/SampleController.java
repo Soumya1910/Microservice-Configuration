f6f5dce05b122eed326ceb75725f7f66b0b361c5
Soumya Mukherjee <rivu10@gmail.com>
2021-11-14 11:47:12 +0530

    Run application from internal and external application.properties file

---
+++ microservice-configuration/src/main/java/com/microservice/controller/SampleController.java
@@ -0,0 +1,20 @@
+package com.microservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class SampleController {
+	
+	@Value("${user.greetingMessage}")
+	private String greetingMessage;
+	
+	@GetMapping("/")
+	public String getGreeting() {
+		return greetingMessage;
+	}
+
+}
